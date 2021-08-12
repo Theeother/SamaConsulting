032a39735b4c6c825d5faac2f2d2ce2cd734cd1a
Seddik <ka.seddik@gmail.com>
2021-08-12 02:45:52 +0100

    Importing done

--- src/app/components/edit-products/edit-products.component.ts
+++ src/app/components/edit-products/edit-products.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { IProduct } from '../../models/product';
 import { NgForm } from '@angular/forms';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-edit-products',
   templateUrl: './edit-products.component.html',
@@ -17,12 +17,16 @@ export class EditProductsComponent implements OnInit {
   newLaptop: any = {};
   editLaptopForm: boolean;
   editedLaptop: any={};
+  product:any=[];
+  submitbut:boolean=false;
+  lapToAdd=<IProduct>{};
 
   constructor(private _productService : ProductsService) {
   }
 
   ngOnInit(): void {
     this.getLaptopData();
+    
   }
 
   getLaptopData(): void {
@@ -85,5 +89,66 @@ export class EditProductsComponent implements OnInit {
   cancelNewLaptop() {
     this.newLaptop = {};
     this.laptopForm = false;
+  }
+
+
+  ///////////////////// Importation
+
+  onFileChange(evt: any){
+    const target:DataTransfer = <DataTransfer>(evt.target);
+
+    const reader : FileReader = new FileReader();
+
+    reader.onload = (e:any)=>{
+      const bstr : string = e.target.result;
+
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+
+      const wsname: string = wb.SheetNames[0];
+
+      const ws :XLSX.WorkSheet = wb.Sheets[wsname];
+
+      //console.log(ws);
+      //console.log((XLSX.utils.sheet_to_json(ws)));
+      this.product = (XLSX.utils.sheet_to_json(ws));
+      console.log(this.product);
+    };
+
+    reader.readAsBinaryString(target.files[0]);
+    this.submitbut=true;
+    //console.log(this.product);
+  }
+
+  submit(){
+
+    for (let lap of this.product){
+      this.lapToAdd.category=lap.Categorie;
+      this.lapToAdd.brand=lap.Marque;
+      this.lapToAdd.model=lap.Modele;
+      this.lapToAdd.series=lap.N_de_Serie;
+      this.lapToAdd.screenSize=lap.Taille_Ecran;
+      this.lapToAdd.cpuModel=lap.CPU;
+      this.lapToAdd.cpuFreq=lap.Freq;
+      this.lapToAdd.ramType=lap.RAM;
+      this.lapToAdd.ramSize=lap.Taille_Ram;
+      this.lapToAdd.gpuBrand=lap.GPU;
+      this.lapToAdd.OS=lap.OS;
+      this.lapToAdd.price=lap.Prix;
+      this.lapToAdd.urlImage="/assets/images/laptops/"+lap.Url_image;
+      this.lapToAdd.stock=lap.Stock;
+
+      console.log(this.lapToAdd);
+      
+      this.saveLaptop(this.lapToAdd);
+      this.laptopsList=[...this.laptopsList,this.lapToAdd];
+      this.lapToAdd=<IProduct>{};
+      
+
+
+
+
+    }
+    
+
   }
 }
